b567b1800f6bf4d16e9432581679de6b57d54d20
Wren West <wrenwestart@gmail.com>
2025-05-07 09:40:06 -0400

    edited server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,7 +24,7 @@ const server = new ApolloServer({
   resolvers,
 });
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   credentials: true,
   allowedHeaders: ['Authorization', 'Content-Type'],
 }));
@@ -61,11 +61,12 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-    app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
   }
   
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
